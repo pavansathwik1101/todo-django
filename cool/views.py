f4598003f73bc44f107ba7a107e4735571b9a883
Pavan Sathwik <87928430+pavansathwik1101@users.noreply.github.com>
2024-01-23 17:58:29 +0530

    first commit

---
+++ cool/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Member
+
+# Create your views here.
+def index(request):
+    member=Member.objects.all().values()
+    context={'member':member}
+    return render(request,'index.html',context)
+def add_member(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        member = Member(name=name)
+        member.save()
+
+        return redirect('index')  # Redirect to the index page after adding a member
+
+    return render(request, 'index.html')
