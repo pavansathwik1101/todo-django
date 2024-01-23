5077e2e4cb469ae07bd50dc569f34c0ca6f42fc7
Pavan Sathwik <87928430+pavansathwik1101@users.noreply.github.com>
2024-01-24 00:44:39 +0530

    Update urls.py

--- cool/urls.py
+++ cool/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('add_member/', add_member, name='add_member'),
+    path('delete_member/<int:member_id>/', delete_member, name='delete_member'),
 ]
